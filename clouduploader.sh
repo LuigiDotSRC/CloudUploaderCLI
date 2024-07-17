a146b690170e00087d4aaadf1e76bce9c45ce45e
LuigiDotSRC <luigi.terashima@gmail.com>
2024-07-16 22:36:12 -0400

    Seperate initial checks from main script

--- clouduploader.sh
+++ clouduploader.sh
@@ -1,39 +1,22 @@
 #!/bin/bash
 
-# USAGE: clouduploader /path/to/file.txt
+# USAGE: clouduploader /path/to/file.txt container-name [OPTION]...
+
+bash ./init_checks.sh $@ 
+if [ ! $? -eq 0 ]; then 
+    exit 1
+fi
 
 FILENAME=$1
-
-# TODO: move all if statements to init_checks.sh 
-
-if [ ! "$FILENAME" ]; then
-    echo "USAGE: clouduploader /path/to/file.txt"
-    exit 1
-fi
-
-if [ ! -f $FILENAME ]; then
-    echo "File does not exist" 
-    exit 1
-fi
-
-if [ -f .env ]; then
-    source .env
-else 
-    echo ".env file not found"
-    exit 1
-fi
-
-if [ ! "$SUBSCRIPTION_ID" -o ! "$ACCOUNT_NAME" -o ! "$AZURE_STORAGE_KEY" ]; then
-    echo ".env not configured correctly"
-    exit 1
-fi
+CONTAINERNAME=$2
+source .env
 
 # filename without ./ or ../ 
 NAME=$(echo "$FILENAME" | sed -e 's@\.\./@@g' -e 's@\.\/@@g' -e 's@.*/@@')
 cp "$FILENAME" "$NAME"
 
-# TODO: add container name param 
-az storage blob upload --account-name $ACCOUNT_NAME --container-name files --name $NAME --file $NAME --auth-mode key --account-key $AZURE_STORAGE_KEY
+az storage blob upload --account-name $ACCOUNT_NAME --container-name $CONTAINERNAME --name $NAME --file $NAME --auth-mode key --account-key $AZURE_STORAGE_KEY
+echo $?
 
 # cleanup copied file
 rm "$NAME"
---
+++ init_checks.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+FILENAME=$1
+CONTAINERNAME=$2
+
+if [ ! "$FILENAME" -o ! "$CONTAINERNAME" ]; then
+    echo "USAGE: clouduploader /path/to/file.txt container-name [OPTION]..."
+    exit 1
+fi
+
+if [ ! -f $FILENAME ]; then
+    echo "File does not exist" 
+    exit 1
+fi
+
+if [ -f .env ]; then
+    source .env
+else 
+    echo ".env file not found"
+    exit 1
+fi
+
+if [ ! "$SUBSCRIPTION_ID" -o ! "$ACCOUNT_NAME" -o ! "$AZURE_STORAGE_KEY" ]; then
+    echo ".env not configured correctly"
+    exit 1
+fi
+
+exit 0 
